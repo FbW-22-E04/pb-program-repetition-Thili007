6258efcf5223de3fa680941e876da61b54966719
Your Name <tpunchihewage@gmail.com>
2022-09-06 22:24:02 +0200

    finised works

---
+++ index.js
@@ -0,0 +1,33 @@
+// Exercise 01
+
+console.log("-------------------------------------------Exercise 01");
+let i1 = 0;
+
+while (i1 <= 15) {
+  console.log("Printed the number -->", i1++);
+}
+
+// Exercise 02
+
+console.log("-------------------------------------------Exercise 02");
+
+counter = 0;
+total = 0;
+while (counter < 20) {
+  counter += 1;
+  console.log("The Numbers -->", counter);
+  total += counter;
+}
+
+console.log("Total of between 1 num 20 is --->", total);
+
+// Exercise 03
+
+console.log("-------------------------------------------Exercise 03");
+
+let i = 0;
+
+do {
+  console.log(`The number is ${i}`);
+  i++;
+} while (i <= 20);
